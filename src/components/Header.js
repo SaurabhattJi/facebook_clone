1b11b558756e9c8894a26a8db212020da48ed85c
Saurabh Bhatt <saurabhbhatt1999@gmail.com>
2023-03-31 15:00:21 +0530

    header section completed

---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "../css/header.css";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOndemandVideo } from "react-icons/md";
+import { HiUserGroup } from "react-icons/hi";
+import { MdOutlineSportsEsports } from "react-icons/md";
+import { RxAvatar } from "react-icons/rx";
+import { AiFillAppstore } from "react-icons/ai";
+import { BiMessageDots } from "react-icons/bi";
+import { IoIosNotifications } from "react-icons/io";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"
+          alt="Logo"
+        />
+        <div className="header__search">
+          <BsSearch />
+          <input type="text" placeholder="Search here" />
+        </div>
+      </div>
+      <div className="header__middle">
+        <div className="header__option header__option--active">
+          <AiOutlineHome />
+        </div>
+        <div className="header__option">
+          <MdOndemandVideo />
+        </div>
+        <div className="header__option">
+          <HiUserGroup />
+        </div>
+        <div className="header__option">
+          <MdOutlineSportsEsports />
+        </div>
+      </div>
+      <div className="header__right">
+        <div className="header__info">
+          <span>
+            <RxAvatar />
+          </span>
+          <h4>Saurabh Bhatt</h4>
+        </div>
+        <div className="icon">
+          <AiFillAppstore />
+        </div>
+        <div className="icon">
+          <BiMessageDots />
+        </div>
+        <div className="icon">
+          <IoIosNotifications />
+        </div>
+        <div className="icon">
+          <IoMdArrowDropdownCircle />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
